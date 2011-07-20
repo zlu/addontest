ce7a79a2debea0d51972a860a934246e32d5ec45
James Mead <james@floehopper.org>
2011-07-20 12:54:49 +0100

    The worker is probably trying to talk to Resque/Redis, so just to see if this is the case, we're borrowing the Redis config from openvoice2.

--- lib/answer.rb
+++ lib/answer.rb
@@ -3,6 +3,7 @@ require 'connfu'
 uri = URI.parse(ENV['CONNFU_URL'])
 jid = "#{uri.userinfo}@#{uri.host}"
 Connfu.setup jid, ENV['CONNFU_PASSWD']
+Connfu.redis_uri = "redis://redistogo:73d5d351c2595cc21b9d9baef231ccc6@catfish.redistogo.com:9458/"
 
 class AnswerExample
   include Connfu::Dsl
